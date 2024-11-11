3971ee29d05231fb1fc47525e0faa1d071f8d12b
willcode07 <141064556+willcode07@users.noreply.github.com>
2024-11-11 12:41:18 -0500

    Add files via upload

---
+++ app_page.tsx
@@ -0,0 +1,5 @@
+import ComingSoonPage from '../components/coming-soon'
+
+export default function Home() {
+  return <ComingSoonPage />
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+}
+
+module.exports = nextConfig
